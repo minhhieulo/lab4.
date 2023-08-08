dfc33421cd06a186174203577cfe6b0aedcf685d
minhhieulo <124852472+minhhieulo@users.noreply.github.com>
2023-08-08 16:46:34 +0700

    Add files via upload

---
+++ Bai8,9,10(L4).cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<string.h>
+typedef struct NhanVien(){
+	string HoTen,DiaChi;
+	int MaNV;	
+} NhanVien ;
+int Nhap(NV , s[],int &n){
+	for(int i= 0; i<n ; i++){
+		printf("Nhap Vao Ma So Sinh Vien ");
+		scanf("%d",s[i].MaNV);
+    	printf("Nhap vao Ho Ten nhan vien:  ");
+		scanf("%s",s[i].HoTen);
+		printf("Nhap Vao Dia Chi : ");
+		scanf("%s",s[i].DiaChi);
+		
+	}
+}
+voud Xuat(NhanVien ,s[],int &n){
+	for(int i =0; i<n; i++){
+		if(NhanVien==1){
+			
+			printf("")
+		}
+		printf("Ma Nhan Vien la : \n",s[i].MaMV);
+		printf("Nhap Vao ho Ten Nhan Vien : \n ",s[i].HoTen);
+		printf("Dia chi la: \n",s[i].DiaChi);
+	
+}
+int main(){
+	int n ;
+	printf("Nhap Vao so Luong Nhan Vien");
+	for(int i =0 ; i<n ;i++){
+		
+		
+	}
+
+
+	
+	
+	
